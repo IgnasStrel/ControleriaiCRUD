8dacf3f2d55f579ec5f552e5603c65dac2645186
MarcinkEve <marcinke@gmail.com>
2023-06-16 13:55:35 +0300

    namu darbus pasidaryt reiks

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ app.use(express.json())
 // @route POST /api/users
 
 // @access PUBLIC
-
+//CREATE
 const createUser = async (req, res) => {
     if(!req.body.name || !req.body.email || !req.body.password) res.status(404).send("not found");
     const user = new User ({
@@ -23,19 +23,39 @@ const createUser = async (req, res) => {
     const result = await user.save();
     res.status(200).send(result)
 }
+//SELECT VISUS
 const getUsers = async (req, res) => {
     const result = await User.find();
     res.send(result);
 }
-
+//SELECT TIK PAGAL NAME
+const getUserByName = async (req, res) => {
+    const result = await User.find({name: req.body.name});
+    res.send(result);
+}
+//SELECT TIK PAGAL ID
+const getUserById = async (req, res) => {
+    const result = await User.find({_id: req.params.id});
+    res.send(result);
+}
+//DELETE TIK PAGAL ID
 const deleteUser = async (req, res) => {
-    const result = await User.find()
+    const result = await User.deleteOne({_id: req.params.id})
 
-    const user_index = User.indexOf(result)
-    User.splice(user_index, 1);
+    res.send(result)
+}
+//UPDATE TIK PAGAL ID
 
-    res.send(XPathResult)
+const updateUser = async (req, res) => {
+    const result = await User.updateOne({
+        _id: req.params.id}, {
+            $set: {
+                name: req.body.name,
+                password: req.body.password
+            }
+        });
+res.send(result)
 }
 module.exports = {
-    createUser, getUsers, deleteUser
+    createUser, getUsers, getUserByName, getUserById, updateUser, deleteUser
 }
--- index.js
+++ index.js
@@ -10,12 +10,19 @@ app.use(express.json())
 const {
     createUser,
     getUsers,
+    getUserByName,
+    getUserById,
+    updateUser,
     deleteUser
+
 } = require('./controllers/userController');
 
 app.post('/api/user', createUser);
 app.get('/api/users', getUsers);
-app.delete('/api/user', deleteUser);
+app.get('/api/userbyname', getUserByName);
+app.get('/api/user/:id', getUserById);
+app.put('/api/user/:id', updateUser)
+app.delete('/api/user/:id', deleteUser);
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running on PORT ${process.env.PORT}`)
 });
